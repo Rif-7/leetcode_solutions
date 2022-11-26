0649860b3094335e9f1ca1d3c444ee642d6fae0c
Rif-7 <muhammadrifan974530@gmail.com>
2022-11-26 17:25:29 +0530

    add regExMatching solution

---
+++ regExMatching.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        cache = {}
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+            
+            cache[(i, j)] = False
+            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
+            if (j + 1) < len(p) and p[j+1] == "*":
+                cache[(i, j)] = (
+                    dfs(i, j+2) or (match and dfs(i+1, j))
+                )
+            elif match:
+                cache[(i, j)] = dfs(i+1, j+1)
+            
+            return cache[(i, j)]
+        return dfs(0, 0)
