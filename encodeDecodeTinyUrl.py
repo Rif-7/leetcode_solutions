4a21566afca032fd7517cffa6fb607a8bfb92b07
Rif-7 <muhammadrifan974530@gmail.com>
2022-12-16 22:10:31 +0530

    add encodeDecodeTinyUrl solution

---
+++ encodeDecodeTinyUrl.py
@@ -0,0 +1,24 @@
+class Codec:
+    def __init__(self):
+        self.encodeMap = {}
+        self.decodeMap = {}
+        self.baseUrl = "http://tinyurl.com/"
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        if longUrl not in self.encodeMap:
+            shortUrl = self.baseUrl + str(len(self.encodeMap))
+            self.decodeMap[shortUrl] = longUrl
+            self.encodeMap[longUrl] = shortUrl
+        return shortUrl
+        
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        return self.decodeMap[shortUrl]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
