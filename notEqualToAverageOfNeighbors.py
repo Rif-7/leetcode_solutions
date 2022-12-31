4676aa73fc579791bda668fb8e6e147412405e66
Rif-7 <muhammadrifan974530@gmail.com>
2022-12-31 13:33:01 +0530

    add notEqualToAverageOfNeighbors solution

---
+++ notEqualToAverageOfNeighbors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rearrangeArray(self, nums):
+        nums.sort()
+        res = []
+        l = 0
+        r = len(nums) - 1
+        while len(res) != len(nums):
+            res.append(nums[l])
+            l += 1
+            
+            if l <= r:
+                res.append(nums[r])
+                r -= 1
+        return res
+            
