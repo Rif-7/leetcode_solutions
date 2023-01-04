74fae418c5a3e1316bbc05545f350d7f02615639
Rif-7 <muhammadrifan974530@gmail.com>
2023-01-04 18:37:00 +0530

    add asteroidCollision solution

--- asteroidCollision.py
+++ asteroidCollision.py
@@ -2,26 +2,20 @@ class Solution:
     def asteroidCollision(self, asteroids):
         stack = []
         for a in asteroids:
-            if stack and stack[-1] > 0 and a < 0:
-                flag = True
-                while stack:
-                    if stack[-1] > 0 and a < 0:
-                        if abs(a) < abs(stack[-1]):
-                            flag = False
-                            break
-                        elif abs(stack[-1]) < abs(a):
-                            stack.pop()
-                        else:
-                            flag = False
-                            stack.pop()
-                            break
-                    
-                    else:
-                        break
-                if flag:
-                    stack.append(a)
-            
-            else:
+            flag = True
+            while stack and stack[-1] > 0 and a < 0:
+                if abs(a) < abs(stack[-1]):
+                    flag = False
+                    break
+                elif abs(stack[-1]) < abs(a):
+                    stack.pop()
+                else:
+                    flag = False
+                    stack.pop()
+                    break
+ 
+            if flag:
                 stack.append(a)
+
         return stack
         
